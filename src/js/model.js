64ef35f6d68328247461a23fcd09447b6da34d61
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-15 21:08:17 +0200

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,7 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { API_URL, KEY } from './config';
 import { RES_PER_PAGE } from './config';
-import { getJSON, sendJSON } from './helpers';
+import { AJAX } from './helpers';
 
 export const state = {
   recipe: {},
@@ -32,7 +32,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some((bookmark) => bookmark.id === id))
@@ -48,7 +48,7 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
 
-    const data = await getJSON(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}`);
     const { recipes } = data.data;
 
     state.search.results = recipes.map((rec) => {
@@ -139,7 +139,7 @@ export const uploadRecipe = async function (newRecipe) {
       ingredients,
     };
 
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmark(state.recipe);
   } catch (err) {
